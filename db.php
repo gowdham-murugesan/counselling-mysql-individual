b0b1b98521a89537cdb8d3d8a04f62457ce7274e
Gowdham2004 <gowdhammurugesh24@gmail.com>
2022-08-10 22:04:12 +0530

    Updated db to remotemysql

--- db.php
+++ db.php
@@ -3,10 +3,15 @@
     session_start();
   }
   
-  $servername = "sql6.freemysqlhosting.net"; /* Host name */
-  $user = "sql6434984"; /* User */
-  $password = "vA3VgpXckB"; /* Password */
-  $database = "sql6434984"; /* Database name */
+  // $servername = "sql6.freemysqlhosting.net"; /* Host name */
+  // $user = "sql6434984"; /* User */
+  // $password = "vA3VgpXckB"; /* Password */
+  // $database = "sql6434984"; /* Database name */
+
+  $servername = "remotemysql.com"; /* Host name */
+  $user = "WTTjuFFAHW"; /* User */
+  $password = "Ob6CX7wiwc"; /* Password */
+  $database = "WTTjuFFAHW"; /* Database name */
 
   // $user = 'root';
   // $password = 'santhosh'; 
